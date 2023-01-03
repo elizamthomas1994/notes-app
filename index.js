e85ea43d140205c04bd57078cf3cbdc1b30ce7b5
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-03 14:54:33 +0000

    Created and test-drove notesView.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const NotesModel = require('./notesModel');
+const NotesView = require('./notesView');
 
 console.log("The notes app is running")
 
 const model = new NotesModel();
-console.log(model.getNotes());
+model.addNote('This is an example note');
+
+const view = new NotesView(model);
+view.displayNotes();
