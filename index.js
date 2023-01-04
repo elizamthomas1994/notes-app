e5e71680add8194b511e0e6916d8d65b2415c215
Eliza Thomas <elizamthomas1994@gmail.com>
2023-01-04 14:43:02 +0000

    Added NotesClient class

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = 3000;
+const NotesModel = require('./notesModel');
+const NotesView = require('./notesView');
 
-app.use(cors())
+console.log("The notes app is running")
 
-let notes = [
-  'This note is coming from the server'
-];
+const model = new NotesModel();
+console.log(model.getNotes());
+model.addNote('This is an example note');
 
-app.use(express.json());
-
-app.get('/notes', (_req, res) => {
-  res.send(JSON.stringify(notes));
-});
-
-app.post('/notes', (req, res) => {
-  notes.push(req.body.content)
-  res.send(JSON.stringify(notes));
-});
-
-app.delete('/notes', (req, res) => {
-  notes = [];
-  res.send(JSON.stringify(notes))
-});
-
-app.listen(PORT);
+const view = new NotesView(model);
+view.displayNotes();
